ab0615f7cb2f1ef040dc54c0eb6012e40660b938
marvikomo <marvikomo@gmail.com>
2022-05-26 21:50:23 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let server = Server::new("127.0.0.1".to_string());
+     server.run();
+}
+
+struct Server {
+    addr: String,
+
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self{
+            addr
+        }
+    }
+    fn run(self){
+        println!("listening on {}", self.addr);
+    }
+}
