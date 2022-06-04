57f1179d066fa8957e3bb5e9f4a0e7618a976cd9
marvikomo <marvikomo@gmail.com>
2022-06-04 22:23:12 +0100

    status_code

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -2,8 +2,9 @@ pub use method::Method;
 pub use request::Request;
 pub use request::ParseError;
 pub use query_string::{QueryString, Value as QueryStringValue};
-
+pub use response::Response;
 
  pub mod request;
  pub mod method;
  pub mod query_string;
+ pub mod response;
--- src/http/response.rs
+++ src/http/response.rs
@@ -1,3 +1,14 @@
+pub enum StatusCode{}
 pub struct Response{
-    
+    status_code: StatusCode,
+    body: Option<String>
+}
+
+impl Response {
+    pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
+        Response{
+         status_code,
+         body
+        }
+    }
 }
