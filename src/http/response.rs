db342d898562f90212ab545c79169445a286a57b
marvikomo <marvikomo@gmail.com>
2022-06-05 13:00:25 +0100

    wrote directly to stream

--- src/http/response.rs
+++ src/http/response.rs
@@ -13,17 +13,13 @@ impl Response {
     pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
         Response { status_code, body }
     }
-    pub fn send(&self, stream: &mut TcpStream);
-}
-
-impl Display for Response {
-    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+    pub fn send(&self, stream: &mut TcpStream) -> IoResult<()>{
         let body = match &self.body {
             Some(i) => i,
             None => "",
         };
         write!(
-            f,
+            stream,
             "HTTP/1.1 {} {}\r\n\r\n{}",
             self.status_code,
             self.status_code.reason_phrase(),
@@ -31,3 +27,19 @@ impl Display for Response {
         )
     }
 }
+
+// impl Display for Response {
+//     fn fmt(&self, f: &mut Formatter) -> FmtResult {
+//         let body = match &self.body {
+//             Some(i) => i,
+//             None => "",
+//         };
+//         write!(
+//             f,
+//             "HTTP/1.1 {} {}\r\n\r\n{}",
+//             self.status_code,
+//             self.status_code.reason_phrase(),
+//             body
+//         )
+//     }
+// }
