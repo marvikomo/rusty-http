c8588c3b7f8ed31fbc16758ea656573e74959850
marvikomo <marvikomo@gmail.com>
2022-06-06 15:36:28 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,17 @@ use server::Server;
 use http::Request;
 use http::Method;
 use website_handler::WebsiteHandler;
+use std::env;
 
 mod server;
 mod http;
 mod website_handler;
 
 fn main() {
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("public path: {}", public_path);
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
 
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,7 +1,21 @@
 use super::server::Handler;
 use super::http::{Request, Response, StatusCode, Method};
+use std::fs;
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler{
+    public_path: String
+}
+
+impl WebsiteHandler{
+    pub fn new (public_path: String) -> Self{
+        Self {public_path}
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
